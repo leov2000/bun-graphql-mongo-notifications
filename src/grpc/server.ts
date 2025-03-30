ad2b80f0890ab5751d91e36b03304a61d665ddf8
leo <leovergarah@gmail.com>
2025-03-30 00:47:56 -0400

    Added GRPC Reflection

--- src/grpc/server.ts
+++ src/grpc/server.ts
@@ -2,13 +2,21 @@ import * as grpc from '@grpc/grpc-js';
 import { NotificationServiceService } from './proto/generated/notifications';
 import { notificationServiceImpl } from './handlers';
 import { AppMongoClient } from '../mongodb/mongo-client';
+import * as protoLoader from '@grpc/proto-loader';
+import * as reflection from '@grpc/reflection';
+import * as path from 'path';
 
 export async function initializeGrpcServer(
   mongoClient: AppMongoClient,
   port: string,
 ): Promise<grpc.Server> {
   const server = new grpc.Server();
+  const PROTO_PATHS = path.join(__dirname, '../grpc/proto/notifications.proto');
+  const packageDefinition = protoLoader.loadSync(PROTO_PATHS);
+  const reflectionService = new reflection.ReflectionService(packageDefinition);
+  
   server.addService(NotificationServiceService, notificationServiceImpl(mongoClient));
+  reflectionService.addToServer(server);
 
   await new Promise<number>((resolve, reject) => {
     server.bindAsync(
